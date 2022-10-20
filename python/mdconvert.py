d7bae7c275914d4f98c80f2ba945a51fd3ca99b0
Valentin <valentin.lucet@gmail.com>
2022-10-20 19:02:11 -0400

    add script

---
+++ python/mdconvert.py
@@ -0,0 +1,8 @@
+
+
+
+from mdtools import *
+
+md_to_ls("P028_output.json", image_base_dir="P028", 
+         image_root_url="data/local-files/?d=TrailCamStorage/P028", 
+         generate_config_file = False)
