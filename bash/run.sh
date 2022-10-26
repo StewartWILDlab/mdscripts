631ceffe18c8005b50e5ebb9384fb429f2b7b5b5
Valentin <valentin.lucet@gmail.com>
2022-10-26 10:19:31 -0400

    uncomment

--- bash/run.sh
+++ bash/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+## CLI UTILITY FOR MD RUN
+
 # VARS
 STORAGE_DIR="/media/vlucet/TrailCamST/TrailCamStorage"
 BASE_FOLDER="/home/vlucet/Documents/WILDLab/repos/MDtest/git"
@@ -36,7 +38,7 @@ cd $MD_FOLDER
 
 for DIR in "${DIRS[@]}"; do
 
-    echo "*** RUNNING MD ***"
+    #echo "*** RUNNING MD ***"
     
     RUN_DIR=$STORAGE_DIR/$(basename $DIR)
     echo "Running on directory: $RUN_DIR"
@@ -47,34 +49,35 @@ for DIR in "${DIRS[@]}"; do
     CHECKPOINT_PATH="$(basename $DIR)_checkpoint.json"
     echo $CHECKPOINT_PATH
 
-    # if [ -f "$STORAGE_DIR/$OUTPUT_JSON" ]; then # if output exist, break the loop
+    if [ -f "$STORAGE_DIR/$OUTPUT_JSON" ]; then # if output exist, break the loop
         
-    #     echo "Output file $OUTPUT_JSON exists, moving to the next folder"
-    #     continue
+        echo "Output file $OUTPUT_JSON exists, moving to the next folder"
+        continue
 
-    # elif [ -f "$STORAGE_DIR/$CHECKPOINT_PATH" ]; then # else, if checkpoint exists, use it
+    elif [ -f "$STORAGE_DIR/$CHECKPOINT_PATH" ]; then # else, if checkpoint exists, use it
 
-    #     python $MD_FOLDER/detection/run_detector_batch.py \
-    #         $MD_FOLDER/$MODEL $RUN_DIR $STORAGE_DIR/$OUTPUT_JSON \
-    #         --output_relative_filenames --recursive \
-    #         --checkpoint_frequency $CHECKPOINT_FREQ \
-    #         --checkpoint_path $STORAGE_DIR/$CHECKPOINT_PATH \
-    #         --quiet \
-    #         --resume_from_checkpoint $STORAGE_DIR/$CHECKPOINT_PATH \
-    #         --allow_checkpoint_overwrite #--threshold $THRESHOLD
+        python $MD_FOLDER/detection/run_detector_batch.py \
+            $MD_FOLDER/$MODEL $RUN_DIR $STORAGE_DIR/$OUTPUT_JSON \
+            --output_relative_filenames --recursive \
+            --checkpoint_frequency $CHECKPOINT_FREQ \
+            --checkpoint_path $STORAGE_DIR/$CHECKPOINT_PATH \
+            --quiet \
+            --resume_from_checkpoint $STORAGE_DIR/$CHECKPOINT_PATH \
+            --allow_checkpoint_overwrite #--threshold $THRESHOLD
 
-    # else # else, start new run
-    #     python $MD_FOLDER/detection/run_detector_batch.py \
-    #         $MD_FOLDER/$MODEL $RUN_DIR $STORAGE_DIR/$OUTPUT_JSON \
-    #         --output_relative_filenames --recursive \
-    #         --checkpoint_frequency $CHECKPOINT_FREQ \
-    #         --checkpoint_path $STORAGE_DIR/$CHECKPOINT_PATH \
-    #         --quiet #--threshold $THRESHOLD
-    # fi
+    else # else, start new run
+        python $MD_FOLDER/detection/run_detector_batch.py \
+            $MD_FOLDER/$MODEL $RUN_DIR $STORAGE_DIR/$OUTPUT_JSON \
+            --output_relative_filenames --recursive \
+            --checkpoint_frequency $CHECKPOINT_FREQ \
+            --checkpoint_path $STORAGE_DIR/$CHECKPOINT_PATH \
+            --quiet #--threshold $THRESHOLD
+    fi
 
     echo "*** RUNNING CONVERTER ***"
 
     OUTPUT_JSON_LS="$(basename $DIR)_output_ls.json"
+    echo $OUTPUT_JSON_LS
 
     if [ -f "$STORAGE_DIR/$OUTPUT_JSON_LS" ]; then # if output exist, break the loop
         
@@ -83,10 +86,29 @@ for DIR in "${DIRS[@]}"; do
 
     else
 
-        mdtools convert $STORAGE_DIR/$OUTPUT_JSON $THRESHOLD_FILTER -bd $RUN_DIR \
-            -ru "data/local-files/?d=$(basename $STORAGE_DIR)/$(basename $DIR)"
+        mdtools convert ls $STORAGE_DIR/$OUTPUT_JSON -ct $THRESHOLD_FILTER -bd $RUN_DIR \
+            -ru "data/local-files/?d=$(basename $STORAGE_DIR)/$(basename $DIR)" \
+            --write-coco True
+    fi
+
+    echo "*** RUNNING CSV ***"
+
+    OUTPUT_CSV="$(basename $DIR)_output.csv"
+    echo $OUTPUT_CSV
+
+    if [ -f "$STORAGE_DIR/$OUTPUT_CSV" ]; then # if output exist, break the loop
+        
+        echo "Output file $OUTPUT_JSON_LS exists, moving to the next folder"
+        continue
+
+    else
+        mdtools convert csv $STORAGE_DIR/$OUTPUT_JSON
     fi
 
 done
+
+echo "*** COMBINING CSVs ***"
+
+csvstack *_output.csv > combined.csv
 
 cd $OLD_DIR
