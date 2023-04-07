362cc79796f497113ae87c5a31d8e05922540392
vlucet <valentin.lucet@gmail.com>
2023-04-07 14:45:11 -0400

    remove old code

--- python/mdconvert.py
+++
@@ -1,8 +0,0 @@
-
-
-
-from mdtools import *
-
-md_to_ls("P028_output.json", image_base_dir="P028", 
-         image_root_url="data/local-files/?d=TrailCamStorage/P028", 
-         generate_config_file = False)
